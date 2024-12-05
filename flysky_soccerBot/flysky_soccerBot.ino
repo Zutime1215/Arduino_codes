c0287cf115e5fd49ceb74c380a7bb6c0af3bcc6e
Muhammad Zahid Hossen <63770335+Zutime1215@users.noreply.github.com>
2024-12-06 01:37:30 +0600

    Update flysky_soccerBot.ino

--- flysky_soccerBot/flysky_soccerBot.ino
+++ flysky_soccerBot/flysky_soccerBot.ino
@@ -1,13 +1,17 @@
-#define speedCH 2
 volatile long StartTimeFWD = 0;
 volatile long CurrentTimeFWD = 0;
 volatile long PulsesFWD = 0;
 int PulseWidthFWD = 0;
 
-#define CH1 3
-#define CH2 11
+#define speedCH 2 // White wire || CH3
+#define CH1 3 // Black wire
+#define CH2 11 // Orange wire
+
 int ch1Value;
 int ch2Value;
+
+int turnSpeed = 250;
+int revTurnSpeed = 80;
 
 // /FOR LEFT MOTOR/
 int R1PWM = 6;
@@ -33,6 +37,17 @@ void setup(){
 
 
 void loop() {
+  // static unsigned long lastUpdateTime = 0;
+  // const unsigned long UPDATE_INTERVAL = 50; // 20Hz update rate
+  
+  // // Throttle loop execution
+  // if (millis() - lastUpdateTime < UPDATE_INTERVAL) {
+  //   return;
+  // }
+  // lastUpdateTime = millis();
+
+
+
   if (PulsesFWD < 2000){
     PulseWidthFWD = PulsesFWD;
   }
@@ -41,53 +56,66 @@ void loop() {
   ch1Value = readChannel(CH1);
   ch2Value = readChannel(CH2);
   
-  if (ch2Value > 1600)  { //forward
-    analogWrite(R1PWM, Speed);
-    analogWrite(L1PWM, 0);
-
-    analogWrite(R2PWM, Speed);
-    analogWrite(L2PWM, 0);
-  }
-
-  else if (ch2Value < 1350)  {//backward
+  if (ch2Value > 1600)  { // backward
     analogWrite(R1PWM, 0);
     analogWrite(L1PWM, Speed);
 
     analogWrite(R2PWM, 0);
     analogWrite(L2PWM, Speed);
+
+    // Serial.println("backward");
+
+
   }
 
-  else if (ch1Value > 1600)  {//LEFT
-    analogWrite(R1PWM, 0);
-    analogWrite(L1PWM, Speed);
-
-    analogWrite(R2PWM, Speed);
-    analogWrite(L2PWM, 0);
-  }
-
-  else if (ch1Value < 1350)  {//RIGHT
+  else if (ch2Value < 1350)  { // Forward
     analogWrite(R1PWM, Speed);
     analogWrite(L1PWM, 0);
 
-    analogWrite(R2PWM, 0);
-    analogWrite(L2PWM, Speed);
+    analogWrite(R2PWM, Speed);
+    analogWrite(L2PWM, 0);
+
+    // Serial.println("forward");
   }
 
-  else if ( ((ch1Value > 1470) && (ch1Value < 1490)) && ((ch2Value > 1470) && (ch2Value < 1490)) )  { //STOP
+  else if (ch1Value > 1750)  { // Right
+    analogWrite(R1PWM, 0);
+    analogWrite(L1PWM, revTurnSpeed);
+    
+    analogWrite(R2PWM, turnSpeed);
+    analogWrite(L2PWM, 0);
+
+    // Serial.println("Left");
+  }
+
+  else if (ch1Value < 1250)  { // Left
+    // analogWrite(R1PWM, turnSpeed);
+    analogWrite(R1PWM, turnSpeed);
+    analogWrite(L1PWM, 0);
+
+    analogWrite(R2PWM, 0);
+    analogWrite(L2PWM, revTurnSpeed);
+
+    // Serial.println("Right");
+  }
+
+  else if ( ((ch1Value > 1450) && (ch1Value < 1490)) && ((ch2Value > 1450) && (ch2Value < 1490)) )  { // STOP
     analogWrite(R1PWM, 0);
     analogWrite(L1PWM, 0);
 
     analogWrite(R2PWM, 0);
     analogWrite(L2PWM, 0);
+
+    // Serial.println("Stop");
   }
 
-  // Serial.print("Speed: ");
-  Serial.print(Speed);
-  Serial.print(" ");
-  Serial.print(ch1Value);
-  Serial.print(" ");
-  Serial.println(ch2Value);
-  
+  // Serial.print(Speed);
+  // Serial.print(" ");
+  // Serial.print(ch1Value);
+  // Serial.print(" ");
+  // Serial.println(ch2Value);
+
+   
   // delay(50);
 }
 
@@ -103,4 +131,3 @@ void PulseTimerFWD(){
     StartTimeFWD = CurrentTimeFWD;
   }
 }
-
